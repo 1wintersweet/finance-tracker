d3eed8e7d322e1c36e1545fb53b810f8721606df
Wintersweet <ge_zhang@hotmail.com>
2016-03-13 06:51:54 +0000

    Add stock restriction and listing features

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,10 @@
 class UsersController < ApplicationController
   
   def my_portfolio
+    # get two instance variables to views
+    @user_stocks = current_user.stocks
+    @user = current_user
+    
     
   end
   
--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,6 +1,7 @@
 class Stock < ActiveRecord::Base
   
-  
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
   
   # no instance we do class level method
   
